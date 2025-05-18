b14300015d7ac4f84c851d37e5616613bb8fa338
vicxptrv <viktoriavorontsova.main@gmail.com>
2025-05-18 11:00:45 +0700

    fix: stop after invalid args

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: vvoronts <vvoronts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 10:33:37 by vvoronts          #+#    #+#             */
-/*   Updated: 2025/05/18 10:27:25 by vvoronts         ###   ########.fr       */
+/*   Updated: 2025/05/18 10:59:08 by vvoronts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int	main(int argc, char **argv)
 	t_ctx	*ctx;
 
 	if (validate(argv, argc) == FAIL)
+		return (FAIL);
 	ctx = init(argv);
 	if (ctx == NULL)
 		return (FAIL);
