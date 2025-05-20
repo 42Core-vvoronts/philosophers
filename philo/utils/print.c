08ef95c94e94adbecf801aeb43bab1706367d748
vicxptrv <viktoriavorontsova.main@gmail.com>
2025-05-20 14:09:55 +0700

    add: gather helpers in utils

--- philo/utils/print.c
+++ philo/utils/print.c
@@ -6,7 +6,7 @@
 /*   By: vvoronts <vvoronts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 19:18:50 by vvoronts          #+#    #+#             */
-/*   Updated: 2025/05/19 14:44:38 by vvoronts         ###   ########.fr       */
+/*   Updated: 2025/05/20 13:59:37 by vvoronts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,17 @@ void	writestatus(t_philo *philo, char *str)
 	philo->t_now = gettime(philo->ctx) - philo->ctx->t_start;
 	printf("%011ld %d %s\n", philo->t_now, philo->id, str);
 	mxunlock(philo->ctx->write_lock, philo->ctx);	
+}
+// printf("	%d died set flag true\n", philo->id);
+void	writedeath(t_philo *philo)
+{
+	mxlock(philo->ctx->die_lock, philo->ctx);
+	if (philo->ctx->f_end == true)
+	{
+		mxunlock(philo->ctx->die_lock, philo->ctx);
+		return ;
+	}
+	philo->ctx->f_end = true;
+	writestatus(philo, "is dead");
+	mxunlock(philo->ctx->die_lock, philo->ctx);
 }
--- philo/utils/time.c
+++ philo/utils/time.c
@@ -6,7 +6,7 @@
 /*   By: vvoronts <vvoronts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 18:56:16 by vvoronts          #+#    #+#             */
-/*   Updated: 2025/05/19 11:19:14 by vvoronts         ###   ########.fr       */
+/*   Updated: 2025/05/20 13:58:14 by vvoronts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,38 @@ long    gettime(t_ctx *ctx)
 	}
 	milliseconds = rawtime.tv_sec * 1000 + rawtime.tv_usec / 1000;
 	return (milliseconds);
+}
+
+/**
+ * @brief Enhanced usleep
+ * 
+ * @param philo to sleep
+ * @param t_act duration of current action
+ * 
+ * @return int
+ */
+void	esleep(t_philo *philo, long t_act)
+{
+	long	t_wake;
+
+	philo->t_remain = philo->t_last_meal + philo->ctx->t_die - philo->t_now;
+	if (philo->t_remain <= 0)
+		return ;
+	if (philo->t_remain >= t_act)
+	{
+		usleep(t_act * 700);
+		t_wake = philo->ctx->t_start + philo->t_now + t_act;
+		while (gettime(philo->ctx) < t_wake)
+			usleep(10);
+		return ;
+	}
+	else
+	{
+		usleep(philo->t_remain *700);
+		t_wake = philo->ctx->t_start + philo->t_now + philo->t_remain;
+		while (gettime(philo->ctx) < t_wake)
+			usleep(10);
+		writedeath(philo);
+		return ;	
+	}
 }
